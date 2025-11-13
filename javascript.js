e9cedba359e7556b147a103936ec24a284841757
Ibrahim awab <179867549+miawab@users.noreply.github.com>
2025-11-14 01:29:59 +0500

    rock paper scissors finalized console based rock paper scissors with no ui finalized

--- javascript.js
+++ javascript.js
@@ -1,13 +1,52 @@
 function getComputerChoice() {
     let x = 3*Math.random();
-    switch (Math.floor(x)) {
-        case 0:
-            return 'rock';
-        case 1:
-            return 'paper';
-        case 2:
-            return 'scissors'; 
-    }
+    return Math.floor(x);
 }
 
-console.log(getComputerChoice())
+function getHumanChoice() {
+    let x = prompt("gimme good value boy, rock paper or scissors")
+    x = x.toLowerCase();
+        switch (x) {
+        case 'rock':
+            return 0;
+        case 'paper':
+            return 1;
+        case 'scissors':
+            return 2; 
+    };
+}
+
+let humanScore;
+let computerScore;
+
+function playRound(humanChoice, computerChoice) {   
+    if (humanChoice === computerChoice) {
+        return console.log("Draw!");
+    }
+    let diff = humanChoice - computerChoice;
+    if (diff<0) {
+        diff =  diff + 3;
+    }
+    if (diff==1) {
+        humanScore++
+        return console.log("Win");
+    }
+    else if(diff == 2) {
+        computerScore++
+        return console.log("Loss")
+    }
+    
+}
+function playGame() {
+    humanScore = 0;
+    computerScore = 0;
+    
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    console.log(`Final score is:\n Player: ${humanScore}, Computer: ${computerScore}`)
+}
+
+playGame()
